e2f01615b4bdbb31bed40c2e45361f5f9b26eb7c
LeoKnox <LeoKnox@users.noreply.github.com>
2019-12-20 19:10:49 -0700

    Add files via upload

---
+++ Models/Dungeon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DungeonMaker.Models
+{
+    public class Dungeon
+    {
+        public int ID { get; set; }
+        public string RoomName { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Wall { get; set; }
+    }
+}
