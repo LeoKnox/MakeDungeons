66c4014f2c9ebeffee104981e1598ebfd7b31ec8
LeoKnox <LeoKnox@users.noreply.github.com>
2019-12-20 19:11:38 -0700

    Add files via upload

---
+++ Pages/Dungeons/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DungeonMaker.Models;
+
+namespace DungeonMaker.Pages.Dungeons
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly DungeonMaker.Models.DungeonMakerContext _context;
+
+        public DetailsModel(DungeonMaker.Models.DungeonMakerContext context)
+        {
+            _context = context;
+        }
+
+        public Dungeon Dungeon { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Dungeon = await _context.Dungeon.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Dungeon == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ Pages/Dungeons/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DungeonMaker.Models;
+
+namespace DungeonMaker.Pages.Dungeons
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DungeonMaker.Models.DungeonMakerContext _context;
+
+        public IndexModel(DungeonMaker.Models.DungeonMakerContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Dungeon> Dungeon { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Dungeon = await _context.Dungeon.ToListAsync();
+        }
+    }
+}
