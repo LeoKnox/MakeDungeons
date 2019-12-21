842dca515aa477c28646bc81d74aaf856c8f7320
LeoKnox <LeoKnox@users.noreply.github.com>
2019-12-20 19:10:30 -0700

    Add files via upload

---
+++ Data/DungeonMakerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DungeonMaker.Models
+{
+    public class DungeonMakerContext : DbContext
+    {
+        public DungeonMakerContext (DbContextOptions<DungeonMakerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DungeonMaker.Models.Dungeon> Dungeon { get; set; }
+    }
+}
