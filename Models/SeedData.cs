0dba5c9a3262f554f0021a544a80671c5190a019
LeoKnox <LeoKnox@users.noreply.github.com>
2019-12-21 20:08:12 -0700

    Add files via upload

---
+++ Models/SeedData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DungeonMaker.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new DungeonMakerContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<DungeonMakerContext>>()))
+            {
+                if (context.Dungeon.Any())
+                {
+                    return;
+                }
+
+                context.Dungeon.AddRange(
+                    new Dungeon
+                    {
+                        RoomName = "Second",
+                        Width = 3,
+                        Height = 5,
+                        Wall = "Stone"
+                    },
+
+                    new Dungeon
+                    {
+                        RoomName = "Third",
+                        Width = 4,
+                        Height = 4,
+                        Wall = "Wood"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using DungeonMaker.Models;
 
 namespace DungeonMaker
 {
@@ -14,7 +16,24 @@ namespace DungeonMaker
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
